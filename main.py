5b5c05c96dd3b606823115c5abe02e250c34a6ab
Joshua Robert <Enidiok>
2021-12-14 12:22:18 +0100

    second commit

--- main.py
+++ main.py
@@ -9,4 +9,14 @@ def timeout():
     print("1st timeout is finished")
 
 def timeout():
-    print("2nd timeout ")
+    print("2nd timeout is started")
+    time.sleep(5)
+    print("2nd timeout is finshed")
+
+async def main():
+    print("main is started")
+    threading.Thread(target=timeout).start()
+    threading.Thread(target=timeout).start()
+    print("main is finished")
+
+asyncio.run(main())
